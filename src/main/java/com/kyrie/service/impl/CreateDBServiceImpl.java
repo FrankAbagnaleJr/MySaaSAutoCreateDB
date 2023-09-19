4d7d4eb5483f8380e40ba49a41df53f505a94133
冀金梁 <625103471@qq.com>
2023-09-19 17:53:22 +0800

    把DynamicRoutingDataSource注入方式Autowired修改成Resource

--- src/main/java/com/kyrie/service/impl/CreateDBServiceImpl.java
+++ src/main/java/com/kyrie/service/impl/CreateDBServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -23,7 +24,7 @@ import java.util.Objects;
 @Service
 public class CreateDBServiceImpl implements CreateDBService {
 
-    @Autowired
+    @Resource
     DynamicRoutingDataSource dataSource;
 
     @Autowired
